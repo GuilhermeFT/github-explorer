996f65bb5fdb62dd347d79e34020433716e9160d
Guilherme F. Trindade <38335563+GuilhermeFT@users.noreply.github.com>
2021-07-08 20:04:22 -0300

    Importando arquivos CSS

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   mode: isDevelopment ? 'development' : 'production',
-  devtool: isDevelopment ?  'eval-source-map' : 'source-map',
+  devtool: isDevelopment ? 'eval-source-map' : 'source-map',
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -28,7 +28,12 @@ module.exports = {
         test: /\.jsx$/,
         exclude: /node_modules/,
         use: 'babel-loader',
-      }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
     ]
   }
 }
